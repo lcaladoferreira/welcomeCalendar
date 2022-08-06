baea4a5a7d6519b12fd7f075dd5d0052cdde36c3
Leandro Calado Ferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-08-06 16:18:34 -0300

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class home extends Component {
+  render() {
+    return (
+      <div>
+        <a
+          href="https://scheduler-sooty.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Click here to start
+        </a>
+      </div>
+    );
+  }
+}
